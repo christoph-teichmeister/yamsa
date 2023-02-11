1a152d511b3ed9efbb2b3356d6c5eca4dd258359
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-02-11 13:05:50 +0100

    feat: creates room views

--- apps/room/views.py
+++ apps/room/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from apps.room.models import Room
+
+
+class RoomView(generic.ListView):
+    template_name = "room/list.html"
+    context_object_name = "rooms"
+    model = Room
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if not user.is_anonymous:
+            return Room.objects.filter(created_by=user)
+        return Room.objects.all()
