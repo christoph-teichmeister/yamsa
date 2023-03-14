a9cef204e15445603362ecbf066ac6a8a7cdc049
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-03-14 15:38:58 +0100

    feat: adds UserConnectionToTransaction and changes how paid_for on Transaction is handled

--- apps/transaction/admin.py
+++ apps/transaction/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import register
 
 from apps.core.admin import YamsaCommonInfoAdminMixin
-from apps.transaction.models import Transaction
+from apps.transaction.models import Transaction, UserConnectionToTransaction
 
 
 @register(Transaction)
@@ -11,10 +11,32 @@ class TransactionAdmin(YamsaCommonInfoAdminMixin, admin.ModelAdmin):
         (None, {"fields": ("room", "description", "value")}),
         (
             "People involved",
-            {"fields": ("paid_for", "paid_by")},
+            {
+                "fields": (
+                    # "paid_for",
+                    "paid_by",
+                )
+            },
         ),
         (
             "Status",
             {"fields": ("settled", "settled_at")},
         ),
     )
+
+
+@register(UserConnectionToTransaction)
+class UserConnectionToTransactionAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "transaction", "user")
+    search_fields = ("transaction__name", "user__name")
+    fieldsets = (
+        (
+            None,
+            {"fields": ("user", "transaction")},
+        ),
+    )
+
+
+class UserConnectionToTransactionInline(admin.TabularInline):
+    model = UserConnectionToTransaction
+    extra = 0
--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -4,8 +4,9 @@ from django.db import models
 
 class Transaction(CommonInfo):
     description = models.TextField(max_length=500)
-    paid_for = models.ForeignKey(
-        "account.User", related_name="owes_transactions", on_delete=models.CASCADE
+    paid_for = models.ManyToManyField(
+        "account.User",
+        through="transaction.UserConnectionToTransaction",
     )
     paid_by = models.ForeignKey(
         "account.User", related_name="made_transactions", on_delete=models.CASCADE
@@ -28,4 +29,20 @@ class Transaction(CommonInfo):
         verbose_name_plural = "Transaction"
 
     def __str__(self):
-        return f"{self.paid_by} paid {self.value}€ for {self.paid_for}"
+        return f"{self.paid_by} paid {self.value}€ for {', '.join(self.paid_for.values_list('name', flat=True))}"
+
+
+class UserConnectionToTransaction(models.Model):
+    user = models.ForeignKey(
+        "account.User", on_delete=models.CASCADE, related_name="owes_transactions"
+    )
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "User-Connection to Transaction"
+        verbose_name_plural = "User-Connections to Transactions"
+
+    def __str__(self):
+        return (
+            f"{self.user} owes {self.transaction.value} to {self.transaction.paid_by}"
+        )
