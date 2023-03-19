40d8a6f9dcedce862bee298bccc1f6196c2cc589
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-03-19 18:02:19 +0100

    feat: adds collectstatic to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ echo "" && echo "Run migrations" && echo ""
 python manage.py migrate
 echo "" && echo "Done with migrations!" && echo ""
 
+echo "" && echo "Collect static" && echo ""
+python manage.py collectstatic --noinput
+echo "" && echo "Done with collecting static!" && echo ""
+
 exec "$@"
