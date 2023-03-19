3140653a3de9cf145ba5bf205ff9de6ac87d7e96
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-03-19 18:09:00 +0100

    feat: adjusts context_data of RoomDetailView

--- apps/room/views.py
+++ apps/room/views.py
@@ -1,5 +1,7 @@
+import time
+
+from django.db import connection
 from django.db.models import F
-from django.http import HttpResponse
 from django.views import generic
 
 from apps.room.models import Room
@@ -34,7 +36,11 @@ class RoomDetailView(generic.DetailView):
         room = context_data.get("room")
         room_users = room.users.all().values("name", "id")
 
-        room_transactions_qs = room.transactions.all()
+        room_transactions_qs = (
+            room.transactions.all()
+            .select_related("paid_by", "room")
+            .prefetch_related("paid_for")
+        )
         room_transactions = room_transactions_qs.annotate(
             paid_by_name=F("paid_by__name"), paid_for_name=F("paid_for__name")
         ).values(
@@ -46,18 +52,39 @@ class RoomDetailView(generic.DetailView):
         )
 
         debts = DebtService.get_debts_dict(room_transactions_qs=room_transactions_qs)
+
+        queries_before_tuple = len(connection.queries)
+        start = time.time()
         money_flow_tuple = DebtService.build_money_flow_tuple(
             queryset=room_transactions_qs
         )
+        queries_after_tuple = len(connection.queries)
+        end = time.time()
+        print(
+            f"money_flow_tuple took {end - start} and made {queries_after_tuple - queries_before_tuple} queries\n"
+        )
+
+        queries_before_qs = len(connection.queries)
+        start = time.time()
+        money_flow_qs = DebtService.build_money_flow_queryset(
+            queryset=room_transactions_qs
+        )
+        queries_after_qs = len(connection.queries)
+        end = time.time()
+        print(
+            f"money_flow_qs took {end - start}and made {queries_after_qs - queries_before_qs} queries\n"
+        )
+
+        # money_flow_qs = MoneyFlow.objects.try_to_resolve_flows_and_reduce_them_to_zero(
+        #     room_id=room.id
+        # )
 
         return {
             **context_data,
             "room_users": room_users,
             "room_transactions": room_transactions,
             "debts": debts,
-            "optimised_debts": money_flow_tuple,
+            "money_flow_tuple": money_flow_tuple,
+            "money_flow_qs": money_flow_qs,
+            "shit_qs": money_flow_qs,
         }
-
-    def post(self, *args, **kwargs):
-        print("POSTED")
-        return HttpResponse(status=200)
