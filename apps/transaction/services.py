448cf6b7882f49a0b13fdb0d2b3c87ce948496bc
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-04-10 10:17:38 +0200

    feat: removes old moneyflow tuple creation logic

--- apps/transaction/services.py
+++ apps/transaction/services.py
@@ -1,8 +1,5 @@
 from django.db.models import QuerySet
 
-from apps.moneyflow.models import MoneyFlow as MF
-from apps.moneyflow.querysets import MoneyFlowQuerySet
-from apps.transaction.dataclasses import Debt, DebtTuple, MoneyFlow, MoneyFlowTuple
 from apps.transaction.models import Transaction
 
 
@@ -29,87 +26,3 @@ class DebtService:
                         debts[paid_for_name] = {paid_by_name: value}
 
         return debts
-
-    @staticmethod
-    def build_money_flow_tuple(queryset: QuerySet[Transaction]) -> MoneyFlowTuple:
-        debt_tuple: DebtTuple = DebtTuple()
-
-        # Create a debt_tuple consisting of ALL debts
-        for transaction in queryset:
-            creditor = transaction.paid_by
-
-            for debitor in transaction.paid_for.all():
-                # Find existing exact connection (A owes B from at least TWO transactions)
-                existing_connection_qs = debt_tuple.filter(deb_cred=(debitor, creditor))
-                existing_connection = existing_connection_qs.first()
-
-                if existing_connection:
-                    debt_tuple.update_debt(
-                        existing_connection, amount_owed__add=transaction.value
-                    )
-                else:
-                    if debitor != creditor:
-                        debt_tuple.add_item(
-                            Debt(
-                                debitor=debitor,
-                                creditor=creditor,
-                                amount_owed=transaction.value,
-                            )
-                        )
-                    else:
-                        print(
-                            f"Debitor was about to be their own Creditor {debitor.id=}\n"
-                        )
-
-        money_flow_tuple: MoneyFlowTuple = MoneyFlowTuple()
-
-        # Create a dict of how many each person pays and receives
-        for debt in debt_tuple:
-            existing_debitor_flow_qs = money_flow_tuple.filter(user=debt.debitor)
-            existing_debitor_flow = existing_debitor_flow_qs.first()
-
-            if existing_debitor_flow:
-                money_flow_tuple.update_debt(
-                    existing_debitor_flow, outgoing__add=debt.amount_owed
-                )
-            else:
-                money_flow_tuple.add_item(
-                    MoneyFlow(user=debt.debitor, outgoing=debt.amount_owed)
-                )
-
-            existing_creditor_flow_qs = money_flow_tuple.filter(user=debt.creditor)
-            existing_creditor_flow = existing_creditor_flow_qs.first()
-
-            if existing_creditor_flow:
-                money_flow_tuple.update_debt(
-                    existing_creditor_flow, incoming__add=debt.amount_owed
-                )
-            else:
-                money_flow_tuple.add_item(
-                    MoneyFlow(user=debt.creditor, incoming=debt.amount_owed)
-                )
-
-        # print("\nFLOWS BEFORE OPTIMISING:\n")
-        # money_flow_tuple.print_items()
-
-        [flow.optimise_flows() for flow in money_flow_tuple]
-
-        # print("\nFLOWS AFTER OPTIMISING:\n")
-        # money_flow_tuple.print_items()
-
-        return money_flow_tuple
-
-    @staticmethod
-    def build_money_flow_queryset(queryset: QuerySet[Transaction]) -> MoneyFlowQuerySet:
-
-        room = None
-
-        # Create a debt_tuple consisting of ALL debts
-        for transaction in queryset.reverse():
-            room = transaction.room
-
-            MF.objects.create_or_update_flows_for_transaction(transaction=transaction)
-
-        room.money_flows.all().optimise_incoming_outgoing_values_for_queryset()
-
-        return room.money_flows.all() if room is not None else MF.objects.none()
--- apps/transaction/views.py
+++ apps/transaction/views.py
@@ -1,7 +1,6 @@
 from django.urls import reverse
 from django.views import generic
 
-from apps.moneyflow.models import MoneyFlow
 from apps.transaction.forms import TransactionForm
 from apps.transaction.models import Transaction
 
@@ -13,20 +12,3 @@ class TransactionCreateView(generic.CreateView):
 
     def get_success_url(self):
         return reverse(viewname="room-detail", args=self.request.POST.get("room"))
-
-    def post(self, request, *args, **kwargs):
-        ret = super().post(request, *args, **kwargs)
-
-        # transaction: Transaction = self.object
-        # if transaction is not None:
-        #     transaction.value = transaction.value / transaction.paid_for.count()
-        #     transaction.save()
-        #
-        #     MoneyFlow.objects.create_or_update_flows_for_transaction(
-        #         transaction=transaction
-        #     )
-        #
-        #     existing_flows = MoneyFlow.objects.filter(user_id=transaction.paid_by)
-        #     existing_flows.optimise_incoming_outgoing_values_for_queryset()
-
-        return ret
