32716da6c0d2f88964a0ae058dfa9135ad623d70
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-04-10 18:14:32 +0200

    feat: makes base view redirect to welcome view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,13 @@
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import generic
 
 
 class BaseView(generic.TemplateView):
     template_name = "core/base.html"
+
+    def get(self, request, *args, **kwargs):
+        return redirect(reverse(viewname="core-welcome"))
 
 
 class WelcomePartialView(generic.TemplateView):
