7b239dec42ecb8c6e56edb7b897808821ccfb240
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-04-10 10:16:29 +0200

    feat: writes two AccountViews

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -6,4 +6,14 @@ urlpatterns = [
     path(
         "profile/<int:pk>", views.UserProfileView.as_view(), name="account-user-profile"
     ),
+    path(
+        "guest_user/authenticate/",
+        views.AuthenticateGuestUserView.as_view(),
+        name="account-authenticate-guest-user",
+    ),
+    path(
+        "logout/",
+        views.LogOutUserView.as_view(),
+        name="account-logout",
+    ),
 ]
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 
 from apps.account.models import User
@@ -7,3 +10,34 @@ class UserProfileView(generic.DetailView):
     template_name = "account/detail.html"
     context_object_name = "user"
     model = User
+
+
+class AuthenticateGuestUserView(generic.View):
+    http_method_names = [
+        "post",
+        "options",
+    ]
+
+    def post(self, request, *args, **kwargs):
+        room_id = self.request.POST.get("room_id")
+
+        redirect_response = HttpResponseRedirect(
+            redirect_to=reverse(viewname="room-detail", args=room_id)
+        )
+
+        if request.user.is_authenticated:
+            return redirect_response
+
+        user_id = self.request.POST.get("user_id")
+        guest_user = User.objects.get(id=user_id)
+        login(request=request, user=guest_user)
+
+        return redirect_response
+
+
+class LogOutUserView(generic.View):
+    http_method_names = ["get", "options"]
+
+    def get(self, request, *args, **kwargs):
+        logout(request=request)
+        return HttpResponseRedirect(redirect_to=reverse(viewname="core-welcome"))
