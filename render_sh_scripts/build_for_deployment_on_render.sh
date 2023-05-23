15ddfcd73ed697d1ed295c7abfc1c84016d1d969
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-05-23 21:56:14 +0200

    feat: creates render relevant sh script

---
+++ render_sh_scripts/build_for_deployment_on_render.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Build file for render.com
+
+# Exit on error
+set -o errexit
+
+echo "" && echo "pip install --upgrade pip && pip install -U pip pipenv"
+pip install --upgrade pip && pip install -U pip pipenv
+
+echo "" && echo "pipenv install --system --deploy"
+pipenv install --system --deploy
+
+echo "" && echo "python manage.py collectstatic --noinput"
+python manage.py collectstatic --no-input
+
+echo "" && echo "python manage.py migrate"
+python manage.py migrate
