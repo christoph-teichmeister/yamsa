0dea3332558869fe6a7571f726b2b7a0c1ee5cc1
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-05-30 16:52:00 +0100

    feat: removes YamsaCommonInfoAdminMixin from CurrencyAdmin

--- apps/currency/admin.py
+++ apps/currency/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from apps.core.admin import YamsaCommonInfoAdminMixin
 from apps.currency.models import Currency
 
 
 @register(Currency)
-class CurrencyAdmin(YamsaCommonInfoAdminMixin, admin.ModelAdmin):
+class CurrencyAdmin(admin.ModelAdmin):
     list_display = ("sign", "name")
     search_fields = ("name",)
     fieldsets = (
