bdbd1ba500d28089c8072f657c5529e900a21b8e
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-05-31 13:27:38 +0100

    feat: caches a lot on room detail view

--- apps/room/views/room_detail_view.py
+++ apps/room/views/room_detail_view.py
@@ -1,7 +1,8 @@
 from django.db.models import F
+from django.utils.functional import cached_property
 from django.views import generic
+from django_context_decorator import context
 
-from apps.core.context_managers import measure_time_and_queries
 from apps.currency.models import Currency
 from apps.debt.models import Debt
 from apps.room.models import Room
@@ -12,21 +13,11 @@ class RoomDetailView(generic.DetailView):
     context_object_name = "room"
     model = Room
 
-    @measure_time_and_queries("RoomDetailView.get_context_data()")
-    def get_context_data(self, **kwargs) -> dict:
-        if not self.request.user.is_anonymous:
-            connection = self.object.userconnectiontoroom_set.filter(
-                user_id=self.request.user.id, user_has_seen_this_room=False
-            ).first()
-            if connection is not None:
-                connection.user_has_seen_this_room = True
-                connection.save()
-
-        context_data = super().get_context_data(**kwargs)
-
-        room: Room = context_data.get("room")
-        room_users = (
-            room.userconnectiontoroom_set.all()
+    @context
+    @cached_property
+    def room_users(self):
+        return (
+            self.object.userconnectiontoroom_set.all()
             .select_related("user")
             .values("user_has_seen_this_room")
             .annotate(
@@ -37,26 +28,45 @@ class RoomDetailView(generic.DetailView):
             .order_by("user_has_seen_this_room", "name")
         )
 
+    @context
+    @cached_property
+    def preferred_currency_sign(self):
+        return self.object.preferred_currency.sign
+
+    @context
+    @cached_property
+    def currency_signs(self):
+        return Currency.objects.all()
+
+    @context
+    @cached_property
+    def debts(self):
         debts = {}
-        for user in room.users.all().order_by("name"):
+        for user in self.object.users.all().order_by("name"):
             debts_for_user = Debt.objects.get_debts_for_user_for_room_as_dict(
-                user.id, room.id
+                user.id, self.object.id
             )
             if debts_for_user == {}:
                 continue
 
             debts[user.name] = debts_for_user
+        return debts
 
-        return {
-            **context_data,
-            "room_users": room_users,
-            "debts": debts,
-            "money_flow_qs": room.money_flows.all(),
-            "currency_sign": room.preferred_currency.sign,
-            "currency_signs": Currency.objects.all(),
-        }
+    @context
+    @cached_property
+    def money_flow_qs(self):
+        return self.object.money_flows.all()
 
     def get(self, request, *args, **kwargs):
         ret = super().get(request, *args, **kwargs)
         ret.headers["HX-Trigger-After-Swap"] = "reloadTransactionList"
+
+        if not self.request.user.is_anonymous:
+            connection = self.object.userconnectiontoroom_set.filter(
+                user_id=self.request.user.id, user_has_seen_this_room=False
+            ).first()
+            if connection is not None:
+                connection.user_has_seen_this_room = True
+                connection.save()
+
         return ret
