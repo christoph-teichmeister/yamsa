74d158287f37e92dd1f3559ce8e893990caf0b53
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-06-01 16:53:22 +0100

    feat: adds toaster snippet and functionality

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,9 @@
+import time
+
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views import generic
+from django_context_decorator import context
 
 from apps.news.models import News
 
@@ -23,3 +26,30 @@ class WelcomePartialView(generic.TemplateView):
         context_data["news"] = news_qs.exclude(highlighted=True)
         context_data["highlighted_news"] = news_qs.filter(highlighted=True).first()
         return context_data
+
+
+class ToastHTMXView(generic.TemplateView):
+    template_name = "shared_partials/toast.html"
+
+    @context
+    @property
+    def toast_id(self):
+        return time.time()
+
+    @context
+    @property
+    def toast_message(self):
+        return self.request.GET.get("toast_message")
+
+    @context
+    @property
+    def toast_type(self):
+        toast_type = self.request.GET.get("toast_type", "info")
+
+        toast_type_dict = {
+            "info": "text-bg-primary bg-gradient",
+            "success": "text-bg-success bg-gradient",
+            "warning": "text-bg-warning bg-gradient",
+            "error": "text-bg-danger bg-gradient",
+        }
+        return toast_type_dict.get(toast_type)
