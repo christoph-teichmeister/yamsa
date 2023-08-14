4bce191a3a42a802fb87562c505adb0994ed7aa8
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-08-14 17:30:51 +0200

    feat: calls a first event

--- apps/transaction/forms.py
+++ apps/transaction/forms.py
@@ -3,6 +3,8 @@ from _decimal import Decimal
 from django import forms
 from django.utils import timezone
 
+from apps.core.event_loop.runner import handle_message
+from apps.transaction.messages.commands.transaction import CreateTransaction
 from apps.transaction.models import Transaction
 
 
@@ -30,16 +32,27 @@ class TransactionCreateForm(forms.ModelForm):
         return cleaned_data
 
     def save(self, commit=True):
-        instance = super().save(commit)
+        instance: Transaction = super().save(commit)
 
         # Mark any debts created because of this transaction, which belong to the debitor as settled, as a debitor
         # can not owe themself money
-        instance.paid_by.owes_transactions.filter(
-            user=instance.paid_by, transaction_id=instance.id
-        ).update(settled=True, settled_at=timezone.now())
+        instance.paid_by.owes_transactions.filter(user=instance.paid_by, transaction_id=instance.id).update(
+            settled=True, settled_at=timezone.now()
+        )
 
         from apps.moneyflow.models import MoneyFlow
 
         MoneyFlow.objects.create_or_update_flows_for_transaction(transaction=instance)
+
+        handle_message(
+            CreateTransaction(
+                context_data={
+                    "room": instance.room,
+                    "value": instance.value,
+                    "creditor": instance.paid_by,
+                    "debitor": instance.paid_for,
+                }
+            )
+        )
 
         return instance
