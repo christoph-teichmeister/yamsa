a77cf4e001f7ea0ffae3a58c134d936c5bef5d81
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-09-16 16:29:38 +0200

    fix: removes a comment

--- apps/transaction/handlers/commands/transaction.py
+++ apps/transaction/handlers/commands/transaction.py
@@ -5,10 +5,6 @@ from apps.transaction.messages.events.transaction import TransactionCreated
 
 @message_registry.register_command(command=CreateTransaction)
 def handle_create_transaction(context: CreateTransaction.Context) -> TransactionCreated:
-    # Transaction.objects.create_transaction(
-    #     faction=context.faction, amount=-amount, reason=f"Salaries of {amount} silver paid in week {context.week}."
-    # )
-
     return TransactionCreated(
         context_data={
             "room": context.room,
