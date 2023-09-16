466108b1c8ec983c7976e8a0ff877f4407cf1158
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-09-16 16:30:26 +0200

    feat: updates save method to send a message

--- apps/transaction/forms.py
+++ apps/transaction/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.utils import timezone
 
 from apps.core.event_loop.runner import handle_message
-from apps.transaction.messages.commands.transaction import CreateTransaction
+from apps.transaction.messages.events.transaction import TransactionCreated
 from apps.transaction.models import Transaction
 
 
@@ -40,19 +40,6 @@ class TransactionCreateForm(forms.ModelForm):
             settled=True, settled_at=timezone.now()
         )
 
-        from apps.moneyflow.models import MoneyFlow
-
-        MoneyFlow.objects.create_or_update_flows_for_transaction(transaction=instance)
-
-        handle_message(
-            CreateTransaction(
-                context_data={
-                    "room": instance.room,
-                    "value": instance.value,
-                    "creditor": instance.paid_by,
-                    "debitor": instance.paid_for,
-                }
-            )
-        )
+        handle_message(TransactionCreated(context_data={"transaction": instance}))
 
         return instance
