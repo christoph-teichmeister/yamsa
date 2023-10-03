5fba0bc214432f87584be76011ba9b29a44a226f
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-03 15:02:57 +0200

    feat: rewrites DebtSettleView

--- apps/debt/urls.py
+++ apps/debt/urls.py
@@ -3,9 +3,5 @@ from django.urls import path
 from apps.debt import views
 
 urlpatterns = [
-    path(
-        "settle/",
-        views.DebtSettleView.as_view(),
-        name="transaction-settle",
-    ),
+    path("settle/<int:pk>/", views.DebtSettleView.as_view(), name="transaction-settle"),
 ]
