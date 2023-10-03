77b1a62f0f10e8c5ddbefbd0a6b1dcad9be512f6
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-03 16:15:08 +0200

    feat: edits parse_user_text

--- apps/room/templatetags/room_tags.py
+++ apps/room/templatetags/room_tags.py
@@ -8,13 +8,9 @@ def parse_user_text(context, user_name: str, start_of_sentence: bool = False):
     request = context.get("request")
 
     if request.user.name == user_name:
-        you = "you" if not start_of_sentence else "you".capitalize()
+        you = "you" if not start_of_sentence else "You"
         user_text = f'<span class="me-involved">{you}</span>'
-        if start_of_sentence:
-            user_text += " owe"
     else:
         user_text = f"<strong>{user_name}</strong>"
-        if start_of_sentence:
-            user_text += " owes"
 
     return user_text
