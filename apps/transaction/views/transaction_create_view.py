fbb8fae82f332c6a864cb95296f08d218f35a724
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-14 17:38:17 +0200

    feat: uses FormHtmxResponseMixin in TransactionCreateView

--- apps/transaction/views/transaction_create_view.py
+++ apps/transaction/views/transaction_create_view.py
@@ -1,26 +1,14 @@
-import json
-
-from django.http import HttpResponse
 from django.views import generic
 
 from apps.transaction.forms import TransactionCreateForm
 from apps.transaction.models import ParentTransaction
+from apps.core import htmx
 
 
-class TransactionCreateView(generic.CreateView):
+class TransactionCreateView(htmx.FormHtmxResponseMixin, generic.CreateView):
     model = ParentTransaction
     form_class = TransactionCreateForm
     template_name = "room/detail.html"
 
-    def get_hx_trigger(self):
-        return {
-            "reloadTransactionList": True,
-            "triggerToast": {"message": "Transaction created successfully", "type": "text-bg-success bg-gradient"},
-        }
-
-    def form_valid(self, form):
-        self.object = form.save()
-
-        response = HttpResponse(201)
-        response["HX-Trigger"] = json.dumps(self.get_hx_trigger())
-        return response
+    hx_trigger = "reloadTransactionList"
+    toast_success_message = "Transaction created successfully"
