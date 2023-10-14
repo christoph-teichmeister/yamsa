aeaf0d30abe13c12ea8b48abd34dc9a8609dab9c
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-14 18:05:09 +0200

    feat: deletes model Transaction

--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -3,34 +3,6 @@ from django.db import models
 from django.db.models.aggregates import Sum
 
 
-class Transaction(CommonInfo):
-    description = models.TextField(max_length=500)
-    paid_for = models.ManyToManyField("account.User")
-    paid_by = models.ForeignKey("account.User", related_name="made_transactions", on_delete=models.CASCADE)
-
-    room = models.ForeignKey(
-        "room.Room",
-        on_delete=models.CASCADE,
-        related_name="transactions",
-    )
-
-    value = models.DecimalField(decimal_places=2, max_digits=10)
-
-    currency = models.ForeignKey("currency.Currency", related_name="transactions", on_delete=models.DO_NOTHING)
-
-    class Meta:
-        ordering = ("-id",)
-        verbose_name = "Transaction"
-        verbose_name_plural = "Transaction"
-
-    def __str__(self):
-        multiple_people = self.paid_for.all().count() > 1
-        return (
-            f"{self.paid_by} paid {self.value}{self.currency.sign} for {'each: ' if multiple_people else ''}"
-            f"{', '.join(self.paid_for.values_list('name', flat=True))}"
-        )
-
-
 class ParentTransaction(CommonInfo):
     description = models.TextField(max_length=500)
     paid_by = models.ForeignKey("account.User", related_name="made_parent_transactions", on_delete=models.CASCADE)
