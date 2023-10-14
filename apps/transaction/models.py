4967b135eb1af8ccb01279a4bd189706fc8403f2
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-14 08:52:39 +0200

    feat: creates child and parent transactions in preparation for a feature

--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -1,5 +1,6 @@
 from ambient_toolbox.models import CommonInfo
 from django.db import models
+from django.db.models.aggregates import Sum
 from django.utils import timezone
 
 
@@ -28,4 +29,42 @@ class Transaction(CommonInfo):
         return (
             f"{self.paid_by} paid {self.value}{self.currency.sign} for {'each: ' if multiple_people else ''}"
             f"{', '.join(self.paid_for.values_list('name', flat=True))}"
+        )
+
+
+class ParentTransaction(CommonInfo):
+    description = models.TextField(max_length=500)
+    paid_by = models.ForeignKey("account.User", related_name="made_parent_transactions", on_delete=models.CASCADE)
+
+    room = models.ForeignKey("room.Room", on_delete=models.CASCADE, related_name="parent_transactions")
+
+    currency = models.ForeignKey("currency.Currency", related_name="parent_transactions", on_delete=models.DO_NOTHING)
+
+    class Meta:
+        ordering = ("-id",)
+        default_related_name = "parent_transactions"
+        verbose_name = "Parent Transaction"
+        verbose_name_plural = "Parent Transactions"
+
+    @property
+    def value(self):
+        return self.child_transactions.aggregate(Sum("value"))["value__sum"]
+
+
+class ChildTransaction(CommonInfo):
+    parent_transaction = models.ForeignKey("ParentTransaction", on_delete=models.CASCADE)
+
+    paid_for = models.ForeignKey("account.User", related_name="owes_child_transactions", on_delete=models.CASCADE)
+    value = models.DecimalField(decimal_places=2, max_digits=10)
+
+    class Meta:
+        ordering = ("-id",)
+        default_related_name = "child_transactions"
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"
+
+    def __str__(self):
+        return (
+            f"{self.parent_transaction.paid_by} paid {self.value}{self.parent_transaction.currency.sign} for"
+            f" {self.paid_for}"
         )
