55351dc679017426762b8bce994628d833595eb6
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-14 08:47:06 +0200

    refactor: rewrites DebtAdmin to use NewDebt model

--- apps/debt/admin.py
+++ apps/debt/admin.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from apps.debt.models import Debt
+from apps.debt.models import NewDebt
 
 
-@register(Debt)
-class DebtAdmin(admin.ModelAdmin):
-    list_display = ("id", "__str__", "transaction", "user", "settled")
-    search_fields = ("transaction__name", "user__name")
-    list_filter = ("settled", "transaction__room__name")
+@register(NewDebt)
+class NewDebtAdmin(admin.ModelAdmin):
+    list_display = ("id", "__str__", "debitor", "creditor", "settled", "value")
+    search_fields = ("debitor__name", "creditor__name", "room__name")
+    list_filter = ("settled", "room__name")
     fieldsets = (
         (
             None,
-            {"fields": ("user", "transaction", ("settled", "settled_at"))},
+            {"fields": ("room", ("debitor", "creditor"), ("value", "currency"), ("settled", "settled_at"))},
         ),
     )
 
 
-class DebtInline(admin.TabularInline):
-    model = Debt
+class NewDebtInline(admin.TabularInline):
+    model = NewDebt
     extra = 0
-    fields = ("__str__", "user", "settled", "settled_at")
+    fields = ("__str__", "debitor", "settled", "settled_at")
     readonly_fields = fields
