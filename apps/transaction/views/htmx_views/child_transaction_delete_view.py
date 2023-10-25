3dd4f4f7b2e036f762ce7cdf695cd9beec282571
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-10-25 22:00:22 +0200

    feat: tries to make child_transaction_delete_view.py handle completely deleting a parent_transaction

--- apps/transaction/views/htmx_views/child_transaction_delete_view.py
+++ apps/transaction/views/htmx_views/child_transaction_delete_view.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 
 from apps.core import htmx
@@ -10,3 +12,18 @@ class ChildTransactionDeleteHTMXView(htmx.FormHtmxResponseMixin, generic.DeleteV
     hx_trigger = "reloadTransactionEditView"
     toast_success_message = "Transaction successfully deleted!"
     toast_error_message = "There was an error deleting the transaction"
+
+    def get_response(self):
+        # TODO CT: Nope, this does not work
+        return HttpResponseRedirect(
+            redirect_to=reverse(viewname="room-detail", kwargs={"slug": self.object.parent_transaction.room.slug})
+        )
+
+    def form_valid(self, form):
+        form_valid_return = super().form_valid(form)
+
+        parent_transactions_still_has_child_transactions = self.object.parent_transaction.child_transactions.count() > 0
+        if not parent_transactions_still_has_child_transactions:
+            self.object.parent_transaction.delete()
+
+        return form_valid_return
