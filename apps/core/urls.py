17940ee474da51e2b6a8bf384600ad26c7e8b789
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-11-21 10:18:57 +0100

    feat: removes webpush related code from core app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 
 from apps.core import views
 
 urlpatterns = [
     path("", views.BaseView.as_view(), name="core-base"),
     path("welcome/", views.WelcomePartialView.as_view(), name="core-welcome"),
-    # path("webpush/save/", views.save_info, name="core-webpush-register"),
-    path("webpush/save/", csrf_exempt(views.WebPushSaveView.as_view()), name="core-webpush-register"),
     path("offline/", views.GetUserOfflineTemplateView.as_view(), name="core-offline"),
     path("serviceworker.js", views.ServiceWorkerView.as_view(), name="core-serviceworker"),
     path("manifest.json", views.ManifestView.as_view(), name="core-manifest"),
