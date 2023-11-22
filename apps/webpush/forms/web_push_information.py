c2d78e67ac189a67421db6ee67790e1802eba54c
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-11-22 12:26:57 +0100

    feat: fixes error in save_or_delete

--- apps/webpush/forms/web_push_information.py
+++ apps/webpush/forms/web_push_information.py
@@ -11,12 +11,11 @@ class WebPushInformationForm(forms.ModelForm):
         fields = ("user", "browser", "user_agent", "endpoint", "auth", "p256dh", "status_type")
 
     def save_or_delete(self):
+        status_type = self.cleaned_data.pop("status_type")  # pop status_type from cleaned_data, so get_or_create works
         defaults = {"browser": self.cleaned_data.pop("browser")}
+
         push_info, created = WebpushInformation.objects.get_or_create(**self.cleaned_data, defaults=defaults)
 
         # If unsubscribe is passed, delete the browser and notification info
-        if (
-            self.cleaned_data.pop("status_type") == "unsubscribe"
-            or not self.cleaned_data.get("user").wants_to_receive_webpush_notifications
-        ):
+        if status_type == "unsubscribe" or not self.cleaned_data.get("user").wants_to_receive_webpush_notifications:
             push_info.delete()
