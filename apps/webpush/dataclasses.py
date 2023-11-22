91780acd44ba982721cda2728edaeaa68e650b29
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-11-22 10:53:30 +0100

    feat: refactors Notification dataclass

--- apps/webpush/dataclasses.py
+++ apps/webpush/dataclasses.py
@@ -5,65 +5,72 @@ from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.templatetags.static import static
 
+from apps.account.models import User
+
 
 @dataclass
-class NotificationPayload:
-    """Dataclass for the payload of a notification
+class Notification:
+    """Dataclass for a notification
 
     https://developer.mozilla.org/en-US/docs/Web/API/Notification
     https://web.dev/articles/push-notifications-display-a-notification
 
     Exemplary instantiation:
-
-    NotificationPayload(
-        head="Debug Notification",
-        body="Click me, to go to the welcome page",
-        actions=[
-            {
-                "action": "click-me-action",
-                "type": "button",
-                "title": "Go to user profile",
-                "url": reverse(viewname="account-user-detail", kwargs={"pk": self.request.user.id}),
-            },
-        ],
-        click_url=reverse(viewname="core-welcome"),
-    )
+        notification = Notification(
+            payload=Notification.Payload(
+                head="Test Notification",
+                body="Click me to open your profile page",
+            ),
+        )
     """
 
-    head: str
-    body: str
+    @dataclass
+    class Payload:
+        """Dataclass for payload of a notification"""
 
-    click_url: str = ""
+        head: str
+        body: str
 
-    icon: str = None
-    badge: str = None
-    image: str = None
+        click_url: str = ""
 
-    actions: list[dict] = field(default_factory=lambda: [])
-    vibrate: list[int] = None
-    sound: str = None
+        icon: str = None
+        badge: str = None
+        image: str = None
 
-    @property
-    def _default_icon_and_badge(self):
-        return settings.PROJECT_BASE_URL + static("images/favicon-32x32.png")
+        actions: list[dict] = field(default_factory=lambda: [])
+        vibrate: list[int] = None
+        sound: str = None
 
-    def _build_data(self) -> dict:
-        action_click_urls = []
+        @property
+        def _default_icon_and_badge(self):
+            return settings.PROJECT_BASE_URL + static("images/favicon-32x32.png")
 
-        for action in self.actions:
-            action_click_urls.append({"action": action.get("action", ""), "url": action.pop("url", "")})
+        def _build_data(self) -> dict:
+            action_click_urls = []
 
-        return {"actionClickUrls": action_click_urls, "notificationClickUrl": self.click_url}
+            for action in self.actions:
+                action_click_urls.append({"action": action.get("action", ""), "url": action.pop("url", "")})
 
-    def _set_icon_and_badge_if_empty(self):
-        default_icon_and_badge = settings.PROJECT_BASE_URL + staticfiles_storage.url("images/favicon.ico")
+            return {"actionClickUrls": action_click_urls, "notificationClickUrl": self.click_url}
 
-        if self.icon is None:
-            self.icon = default_icon_and_badge
+        def _set_icon_and_badge_if_empty(self):
+            default_icon_and_badge = settings.PROJECT_BASE_URL + staticfiles_storage.url("images/favicon.ico")
 
-        if self.badge is None:
-            self.badge = default_icon_and_badge
+            if self.icon is None:
+                self.icon = default_icon_and_badge
 
-    def format_for_webpush(self):
-        self._set_icon_and_badge_if_empty()
-        return json.dumps({**self.__dict__, "data": self._build_data()})
+            if self.badge is None:
+                self.badge = default_icon_and_badge
+
+        def format_for_webpush(self):
+            self._set_icon_and_badge_if_empty()
+            return json.dumps({**self.__dict__, "data": self._build_data()})
+
+    payload: Payload
+    ttl: int = 1000
+
+    def send_to_user(self, user: User):
+        from apps.webpush.services import NotificationSendService
+
+        service = NotificationSendService()
+        service.send_notification_to_user(user, self.payload.format_for_webpush(), self.ttl)
