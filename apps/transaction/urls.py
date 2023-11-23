6a01f16916922e3e0b65efae8c4b9c38ae063c26
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-11-23 20:07:59 +0100

    feat: rewrites edit template

--- apps/transaction/urls.py
+++ apps/transaction/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
         views.TransactionDetailHTMXView.as_view(),
         name="htmx-transaction-detail",
     ),
-    path("htmx/edit/<int:pk>", views.TransactionEditHTMXView.as_view(), name="htmx-transaction-edit"),
+    path("<str:room_slug>/htmx/edit/<int:pk>", views.TransactionEditHTMXView.as_view(), name="htmx-transaction-edit"),
     path("htmx/add/", views.ChildTransactionCreateView.as_view(), name="htmx-child-transaction-create"),
     path(
         "htmx/delete/child-transaction/<int:pk>",
