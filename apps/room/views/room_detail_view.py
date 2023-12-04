646f25c966215ace9d7c355753ebaa8d00a1c845
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-04 22:03:06 +0100

    feat: rewrites RoomDetailView

--- apps/room/views/room_detail_view.py
+++ apps/room/views/room_detail_view.py
@@ -1,4 +1,4 @@
-from django.db.models import F
+from django.urls import reverse
 from django.utils.functional import cached_property
 from django.views import generic
 from django_context_decorator import context
@@ -7,35 +7,19 @@ from apps.currency.models import Currency
 from apps.room.models import Room
 
 
-class RoomDetailView(generic.DetailView):
+class RoomDetailView(generic.UpdateView):
     template_name = "room/detail.html"
     context_object_name = "room"
     slug_url_kwarg = "room_slug"
     model = Room
+    fields = ("name", "description", "preferred_currency")
+
+    def get_success_url(self):
+        return reverse("room-detail", kwargs={"room_slug": self.object.slug})
 
     @context
     @cached_property
-    def room_users(self):
-        return (
-            self.object.userconnectiontoroom_set.all()
-            .select_related("user")
-            .values("user_has_seen_this_room")
-            .annotate(
-                name=F("user__name"),
-                id=F("user__id"),
-                is_guest=F("user__is_guest"),
-            )
-            .order_by("user_has_seen_this_room", "name")
-        )
-
-    @context
-    @cached_property
-    def preferred_currency_sign(self):
-        return self.object.preferred_currency.sign
-
-    @context
-    @cached_property
-    def currency_signs(self):
+    def currencies(self):
         return Currency.objects.all()
 
     @context
