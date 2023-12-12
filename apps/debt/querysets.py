18db04468d790950f78b7996a8d1ed08477ae5e1
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-12 21:36:40 +0100

    feat: renames DebtManager and Queryset

--- apps/debt/querysets.py
+++ apps/debt/querysets.py
@@ -3,7 +3,7 @@ from django.db import models
 from apps.account.models import User
 
 
-class NewDebtQuerySet(models.QuerySet):
+class DebtQuerySet(models.QuerySet):
     def filter_for_room_id(self, *, room_id: int):
         return self.filter(room_id=room_id)
 
