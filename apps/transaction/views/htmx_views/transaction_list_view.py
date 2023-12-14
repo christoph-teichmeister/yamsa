09e0b2ab0652e837510fd71a0b03de460f6e79f9
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-14 09:29:23 +0100

    feat: rid TransactionListHTMXView of RoomSpecificMixin

--- apps/transaction/views/htmx_views/transaction_list_view.py
+++ apps/transaction/views/htmx_views/transaction_list_view.py
@@ -1,13 +1,19 @@
 from django.views import generic
+from django_context_decorator import context
 
-from apps.room.views.mixins.room_specific_mixin import RoomSpecificMixin
 from apps.transaction.models import ParentTransaction
 
 
-class TransactionListHTMXView(RoomSpecificMixin, generic.ListView):
+class TransactionListHTMXView(generic.ListView):
     model = ParentTransaction
     context_object_name = "parent_transactions"
     template_name = "transaction/_list.html"
 
     def get_queryset(self):
-        return self.model.objects.filter(room=self._room)
+        return self.model.objects.filter(room=self.request.room)
+
+    @context
+    @property
+    def room(self):
+        # TODO CT: Fix room_url templatetag
+        return self.request.room
