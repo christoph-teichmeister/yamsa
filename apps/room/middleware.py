09d4d66e2e8c754bcd046cc4a640e05d45bb91ec
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-14 09:16:47 +0100

    feat: rewrites middleware to use django magic

--- apps/room/middleware.py
+++ apps/room/middleware.py
@@ -1,3 +1,5 @@
+from django.urls import get_resolver
+
 from apps.room.models import Room
 
 
@@ -7,23 +9,15 @@ class RoomToRequestMiddleware:
         # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
+        # Code to be executed for each request before the view (and later middleware) are called.
 
-        slug_field = filter(lambda field: field.attname == "slug", Room._meta.fields).__next__()
+        resolver_match = get_resolver().resolve(request.path_info)
 
-        # TODO CT: Why are the auto-generated slugs not 32 long but 36 instead?
-
-        cleaned_path_list = list(
-            filter(lambda split_content: len(split_content) == slug_field.max_length + 4, request.path.split("/"))
-        )
-        if len(cleaned_path_list) != 0:
-            room_slug = cleaned_path_list[0]
+        if (room_slug := resolver_match.kwargs.get("room_slug")) is not None:
             setattr(request, "room", Room.objects.get(slug=room_slug))
 
         response = self.get_response(request)
 
-        # Code to be executed for each request/response after
-        # the view is called.
+        # Code to be executed for each request/response after the view is called.
 
         return response
