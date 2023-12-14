dae5d7cda7d796283314a0bc259c535c74e2a276
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-14 09:30:15 +0100

    feat: rid DebtListHTMXView of RoomSpecificMixin

--- apps/debt/views/htmx_views/debt_list_view.py
+++ apps/debt/views/htmx_views/debt_list_view.py
@@ -1,10 +1,10 @@
 from django.views import generic
+from django_context_decorator import context
 
 from apps.debt.models import Debt
-from apps.room.views.mixins.room_specific_mixin import RoomSpecificMixin
 
 
-class DebtListHTMXView(RoomSpecificMixin, generic.ListView):
+class DebtListHTMXView(generic.ListView):
     model = Debt
     context_object_name = "debts"
     template_name = "debt/_list.html"
@@ -13,3 +13,9 @@ class DebtListHTMXView(RoomSpecificMixin, generic.ListView):
         return self.model.objects.filter_for_room_id(room_id=self.request.room.id).order_by(
             "settled", "currency__sign", "debitor__username"
         )
+
+    @context
+    @property
+    def room(self):
+        # TODO CT: Fix room_url templatetag
+        return self.request.room
