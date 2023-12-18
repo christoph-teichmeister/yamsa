057bd6b62c4dcc6c5e4d24f41876d2fa3597608b
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-18 14:36:18 +0100

    feat: cleans up debt list view

--- apps/debt/urls.py
+++ apps/debt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
+from apps.debt.views.debt_list_view import DebtListView
 from apps.debt.views.debt_settle_view import DebtSettleView
-from apps.debt.views.htmx_views.debt_list_view import DebtListView
 from apps.debt.views.money_spent_on_room_view import MoneySpentOnRoomView
 from apps.room.urls import build_room_specific_paths
 
