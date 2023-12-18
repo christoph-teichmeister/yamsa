0d3668037aa4c709c5d0fe1061b0d82dc05c0fb1
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-18 17:08:13 +0100

    feat: splits views

---
+++ apps/core/views/base_view.py
@@ -0,0 +1,10 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views import generic
+
+
+class BaseView(generic.TemplateView):
+    template_name = "core/base.html"
+
+    def get(self, request, *args, **kwargs):
+        return redirect(reverse(viewname="core-welcome"))
