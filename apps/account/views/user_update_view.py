b951da676b1fd249f83dc8da5cc7ab5b88313ce5
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-18 14:10:24 +0100

    feat: splits user edit and detail

--- apps/account/views/user_update_view.py
+++ apps/account/views/user_update_view.py
@@ -1,15 +1,28 @@
+from django.conf import settings
 from django.urls import reverse
 from django.views import generic
+from django_context_decorator import context
+from functools import cached_property
 
 from apps.account.forms import EditUserForm
 from apps.account.models import User
 
 
 class UserUpdateView(generic.UpdateView):
-    template_name = "account/detail.html"
+    template_name = "account/edit.html"
     context_object_name = "user"
     model = User
     form_class = EditUserForm
+
+    @context
+    @cached_property
+    def PROJECT_BASE_URL(self):
+        return settings.PROJECT_BASE_URL
+
+    @context
+    @cached_property
+    def DJANGO_ADMIN_SUB_URL(self):
+        return settings.DJANGO_ADMIN_SUB_URL
 
     def get_success_url(self):
         return reverse(viewname="account-user-detail", kwargs={"pk": self.request.user.id})
