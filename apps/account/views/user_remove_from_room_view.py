2a580eb25dd3e4d9ca55c8336adcaaac4552d06b
christoph-teichmeister <christoph.teichmeister@gmail.com>
2023-12-19 17:28:48 +0100

    feat: sends event on user removal

--- apps/account/views/user_remove_from_room_view.py
+++ apps/account/views/user_remove_from_room_view.py
@@ -1,7 +1,9 @@
 import json
 
+from apps.account.messages.commands.remove_user_from_room import RemoveUserFromRoom
 from apps.account.models import User
 from apps.account.views import UserListForRoomView
+from apps.core.event_loop.runner import handle_message
 
 
 class UserRemoveFromRoomView(UserListForRoomView):
@@ -11,14 +13,21 @@ class UserRemoveFromRoomView(UserListForRoomView):
         user = User.objects.get(id=kwargs.get("pk"))
 
         if user.can_be_removed_from_room(room_id=self.request.room.id):
-            # Convert to event loop
-            self.rooms.remove(self.request.room)
+            handle_message(
+                RemoveUserFromRoom(
+                    context_data={
+                        "room": self.request.room,
+                        "user_to_be_removed": user,
+                        "user_requesting_removal": self.request.user,
+                    }
+                )
+            )
         else:
             response["HX-Trigger-After-Settle"] = json.dumps(
                 {
                     "triggerToast": {
                         "message": f'"{user.name}" can not be removed from this room, because they still have either '
-                        f"transactions or open debts",
+                        f"transactions or open debts.",
                         "type": "text-bg-danger bg-gradient",
                     }
                 }
