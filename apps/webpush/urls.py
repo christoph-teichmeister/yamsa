5788eb3dd9acfec9d3017e2176bd191577557810
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-01-02 14:43:41 +0100

    feat: namespaces webpush urls

--- apps/webpush/urls.py
+++ apps/webpush/urls.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from apps.webpush import views
 
+app_name = "webpush"
 urlpatterns = [
-    path("save/", csrf_exempt(views.WebPushSaveView.as_view()), name="webpush-save"),
+    path("save/", csrf_exempt(views.WebPushSaveView.as_view()), name="save"),
 ]
