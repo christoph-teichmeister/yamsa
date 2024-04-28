f177429fdc760799e40a92d1260bfc9f147da79f
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-04-28 18:26:30 +0200

    feat: create UserAddedToRoomEmailService

---
+++ apps/mail/services/user_added_to_room_mail_service.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.urls import reverse
+
+from apps.mail.services.base_email_service import BaseYamsaEmailService, EmailExtraContext, EmailUserTextContext
+from apps.room.models import Room
+
+
+class UserAddedToRoomEmailService(BaseYamsaEmailService):
+    new_room: Room = None
+
+    def __init__(self, new_room: Room, *args, **kwargs) -> None:
+        self.new_room = new_room
+        super().__init__(*args, **kwargs)
+
+    def get_subject(self) -> str:
+        return f"Welcome to {self.new_room.name} 🥳️"
+
+    def get_email_user_text_context(self):
+        return EmailUserTextContext(
+            text_list=[
+                f"You have been invited to {self.new_room.name} to join your friends 🥳",
+                "Click the button below to view the room!",
+            ]
+        )
+
+    def get_email_extra_context(self):
+        return EmailExtraContext(
+            show_cta=True,
+            cta_btn_link=f"{settings.PROJECT_BASE_URL}{reverse('room:detail', kwargs={'room_slug': self.new_room.slug})}",
+            cta_btn_text="See the room",
+        )
