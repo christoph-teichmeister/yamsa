c1a1a8ec76d2cbe3fe77f7d473a25ea5919353e6
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-04-28 13:50:08 +0200

    feat: fix UserConnectionToRoom creation in UserConnectionToRoomCreateForm

--- apps/room/forms/user_connection_to_room_create_form.py
+++ apps/room/forms/user_connection_to_room_create_form.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 from apps.account.models import User
-from apps.room.models import UserConnectionToRoom
+from apps.room.models import Room, UserConnectionToRoom
 
 
 class UserConnectionToRoomCreateForm(forms.ModelForm):
@@ -17,8 +17,16 @@ class UserConnectionToRoomCreateForm(forms.ModelForm):
         if not User.objects.filter(email=self.cleaned_data["email"]).exists():
             raise ValidationError("Email does not exist")
 
+        return self.cleaned_data["email"]
+
     def clean(self):
         if UserConnectionToRoom.objects.filter(
             user__email=self.data["email"], room__slug=self.data["room_slug"]
         ).exists():
             raise ValidationError({"email": "User with this email address is already in this room"})
+
+    def _post_clean(self):
+        self.instance.user = User.objects.get(email=self.cleaned_data["email"])
+        self.instance.room = Room.objects.get(slug=self.cleaned_data["room_slug"])
+
+        super()._post_clean()
