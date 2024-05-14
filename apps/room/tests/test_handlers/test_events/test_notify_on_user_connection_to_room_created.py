b1ff78f0f7d2b4b8910cbe437c02cbc3c6697cbe
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-05-14 22:32:09 +0200

    feat: write tests

--- apps/room/tests/test_handlers/test_events/test_notify_on_user_connection_to_room_created.py
+++ apps/room/tests/test_handlers/test_events/test_notify_on_user_connection_to_room_created.py
@@ -1,7 +1,9 @@
 from django_pony_express.services.tests import EmailTestService
+from model_bakery import baker
 
 from apps.core.tests.setup import BaseTestSetUp
 from apps.room.models import UserConnectionToRoom
+from apps.webpush.services.notification_send_test_service import NotificationSendTestService
 
 
 class NotifyOnUserConnectionToRoomCreatedTestCase(BaseTestSetUp):
@@ -9,8 +11,23 @@ class NotifyOnUserConnectionToRoomCreatedTestCase(BaseTestSetUp):
     def setUpTestData(cls):
         super().setUpTestData()
         cls.email_test_service = EmailTestService()
+        cls.notification_test_service = NotificationSendTestService()
 
-    def test_test(self):
+    def test_creator_of_room_does_not_receive_an_email_when_creating_room(self):
         UserConnectionToRoom.objects.create(user=self.user, room=self.room, created_by=self.user)
 
-        self.assertEqual(self.email_test_service.filter(to=self.user.email).count(), 0)
+        self.assertEqual(self.email_test_service.all().count(), 0)
+
+    def test_registered_user_invited_to_room_receives_an_email(self):
+        another_user = baker.make_recipe("apps.account.tests.user")
+        UserConnectionToRoom.objects.create(user=another_user, room=self.room, created_by=self.user)
+
+        print(self.notification_test_service.filter(user=another_user))
+
+        self.assertEqual(self.email_test_service.all().count(), 1)
+        self.assertEqual(self.email_test_service.filter(to=another_user.email).count(), 1)
+
+    def test_no_email_is_sent_when_a_guest_is_invited_to_room(self):
+        UserConnectionToRoom.objects.create(user=self.guest_user, room=self.room, created_by=self.user)
+
+        self.assertEqual(self.email_test_service.all().count(), 0)
