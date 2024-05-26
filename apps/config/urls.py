0eda12ecf69642b12453125da2a56a34bc253d94
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-05-26 12:28:16 +0200

    feat: add local media url

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 
@@ -48,4 +50,7 @@ else:
 if settings.DEBUG:
     urlpatterns.append(
         path("__reload__/", include("django_browser_reload.urls")),
+    )
+    urlpatterns.append(
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     )
