fee8f6d719f7c44acf591448132e03962943e83d
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-06-06 14:13:36 +0200

    feat: install kolo

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -135,6 +135,7 @@ LOCAL_APPS = (
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = (
+    "kolo.middleware.KoloMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
