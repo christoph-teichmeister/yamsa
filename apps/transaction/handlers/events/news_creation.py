e6712c94d05d3599e0af582b93fa33cdfd59b7b4
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-06-07 17:00:32 +0200

    chore: write create_news_on_parent_transaction_creation event handler

---
+++ apps/transaction/handlers/events/news_creation.py
@@ -0,0 +1,15 @@
+from apps.core.event_loop.registry import message_registry
+from apps.news.models import News
+from apps.transaction.messages.events.transaction import (
+    ParentTransactionCreated,
+)
+
+
+@message_registry.register_event(event=ParentTransactionCreated)
+def create_news_on_parent_transaction_creation(context: ParentTransactionCreated.Context):
+    News.objects.create(
+        title="New transaction created!",
+        message=f"{context.parent_transaction.created_by.name} paid {context.parent_transaction.value} for '{context.parent_transaction.description}'",
+        room=context.room,
+        created_by=context.parent_transaction.created_by,
+    )
