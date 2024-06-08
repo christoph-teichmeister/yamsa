fd6803a5ca71e006a08fc244e7c905d1b8aa763c
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-06-08 23:10:58 +0200

    feat: add admin action

--- apps/room/admin/room_admin.py
+++ apps/room/admin/room_admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.contrib.admin import register
 
 from apps.core.admin import YamsaCommonInfoAdminMixin
+from apps.debt.services.debt_optimise_service import DebtOptimiseService
 from apps.room.models import Room
+
+
+@admin.action(description="Optimise debts of selected Rooms")
+def optimise_debts_for_room(modeladmin, request, queryset):  # pragma: no cover
+    for room in queryset:
+        DebtOptimiseService.process(room_id=room.id)
 
 
 @register(Room)
 class RoomAdmin(YamsaCommonInfoAdminMixin, admin.ModelAdmin):
+    actions = (optimise_debts_for_room,)
     list_display = ("name", "status")
     list_filter = ("status",)
     search_fields = ("name",)
