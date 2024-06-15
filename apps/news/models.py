39cf4e9b314c4ffaf9be1633265f2cf6e829beff
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-06-15 17:00:48 +0200

    chore: use NewsManager

--- apps/news/models.py
+++ apps/news/models.py
@@ -2,11 +2,14 @@ from ambient_toolbox.models import CommonInfo
 from django.db import models
 
 from apps.core.models.mixins import FullCleanOnSaveMixin
+from apps.news.managers.news import NewsManager
 
 
 class News(FullCleanOnSaveMixin, CommonInfo):
     title = models.CharField(max_length=100)
     room = models.ForeignKey("room.Room", related_name="news", on_delete=models.DO_NOTHING)
+
+    objects = NewsManager()
 
     class Meta:
         verbose_name = "News"
