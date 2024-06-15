75d18f036b71a230482fbd1b11ff21cee9822755
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-06-15 17:00:10 +0200

    chore: create get_attribute template filter

---
+++ apps/core/templatetags/get_attribute.py
@@ -0,0 +1,14 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter("getattribute")
+def getattribute(value, arg):
+    """Gets an attribute of an object dynamically from a string name"""
+
+    if hasattr(value, str(arg)):
+        return getattr(value, arg)
+    else:
+        return settings.TEMPLATE_STRING_IF_INVALID
