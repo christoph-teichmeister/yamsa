9a760e334bc4869f9811747aa664b1cd4dc7e77f
christoph-teichmeister <christoph.teichmeister@gmail.com>
2024-06-15 15:37:23 +0200

    chore: rm js and css of news_card

--- apps/news/components/news_card/news_card.py
+++ apps/news/components/news_card/news_card.py
@@ -4,7 +4,3 @@ from django_components import component
 @component.register("news_card")
 class NewsCard(component.Component):
     template_name = "news_card/template.html"
-
-    class Media:
-        css = "news_card/style.css"
-        js = "news_card/script.js"
--- apps/news/components/news_card/script.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('load', (event) => {
-    console.log("news_card component is fully loaded");
-});
