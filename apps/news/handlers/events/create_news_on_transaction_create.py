8516f77c2b1860da9911b2ba02324b6feafeab7e
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-03-19 11:20:46 +0100

    feat: Add news creation on parent transaction event
    Introduces automatic news entry generation when a parent transaction is created.
    
    - Registered handler for ParentTransactionCreated event.
    - Generates news with transaction details for the associated room.

---
+++ apps/news/handlers/events/create_news_on_transaction_create.py
@@ -0,0 +1,19 @@
+from apps.core.event_loop.registry import message_registry
+from apps.news.models import News
+from apps.transaction.messages.events.transaction import ParentTransactionCreated
+
+
+@message_registry.register_event(event=ParentTransactionCreated)
+def create_news_on_transaction_create(context: ParentTransactionCreated.Context):
+    parent_transaction = context.parent_transaction
+
+    message = (
+        f"{parent_transaction.paid_by.name} paid {parent_transaction.value}{parent_transaction.currency.sign} "
+        f'in "{parent_transaction.room.name}"'
+    )
+
+    News.objects.create(
+        title=f"💸 {parent_transaction.room.display_initials}: Transaction created",
+        message=message,
+        room_id=parent_transaction.room_id,
+    )
