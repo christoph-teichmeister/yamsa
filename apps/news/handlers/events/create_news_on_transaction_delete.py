f8bd2b1d0675d132e40893370db5042053c7eba6
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-03-22 21:10:28 +0100

    feat: Add event for creating news on transaction delete
    Introduces event handling to generate a news entry when a parent transaction is deleted.
    
    - Registered `create_news_on_transaction_deleted` to handle `ParentTransactionDeleted` events.
    - Automatically creates a news entry with relevant details from the deleted transaction.

---
+++ apps/news/handlers/events/create_news_on_transaction_delete.py
@@ -0,0 +1,19 @@
+from apps.core.event_loop.registry import message_registry
+from apps.news.models import News
+from apps.transaction.messages.events.transaction import ParentTransactionDeleted
+
+
+@message_registry.register_event(event=ParentTransactionDeleted)
+def create_news_on_transaction_deleted(context: ParentTransactionDeleted.Context):
+    parent_transaction = context.parent_transaction
+
+    message = (
+        f'{context.user_who_deleted.name} deleted the transaction "{parent_transaction.description}" '
+        f'({parent_transaction.value}{parent_transaction.currency.sign}) in "{parent_transaction.room.name}"'
+    )
+
+    News.objects.create(
+        title=f"🗑️ {parent_transaction.room.capitalised_initials}: Transaction deleted",
+        message=message,
+        room_id=parent_transaction.room_id,
+    )
