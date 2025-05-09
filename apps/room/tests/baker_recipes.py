e74365d8175523eee2d00ace762204652399d921
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-05-09 13:55:15 +0200

    feat: Add foreign key to room recipe
    Enhanced the `room` recipe to include a foreign key for the `created_by` field.
    
    - Imported `user` recipe from `apps.account.tests.baker_recipes`.
    - Updated `room` recipe with `created_by` linked to `user`.

--- apps/room/tests/baker_recipes.py
+++ apps/room/tests/baker_recipes.py
@@ -1,6 +1,12 @@
 from model_bakery import seq
-from model_bakery.recipe import Recipe
+from model_bakery.recipe import Recipe, foreign_key
 
+from apps.account.tests.baker_recipes import user
 from apps.room.models import Room
 
-room = Recipe(Room, name=seq("Vacation "), description="A description")
+room = Recipe(
+    Room,
+    name=seq("Vacation "),
+    description="A description",
+    created_by=foreign_key(user),
+)
