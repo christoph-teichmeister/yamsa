c6f4f2194a11b63539092c87b7855180166a52ed
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-11-17 12:59:15 +0100

    feat: update user views

--- apps/account/views/user_list_for_room_view.py
+++ apps/account/views/user_list_for_room_view.py
@@ -12,7 +12,6 @@ class UserListForRoomView(AccountBaseContext, generic.ListView):
     def get_queryset(self):
         return (
             self.model.objects.get_for_room_slug(room_slug=self.request.room.slug)
-            .values("name", "id", "is_guest")
             .annotate_user_has_seen_this_room()
             .annotate_invitation_email_can_be_sent()
             .order_by("user_has_seen_this_room", "name")
