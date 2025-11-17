d1f8aadc94861832fbe99d9ef66e5ddddfccd95d
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-11-17 13:01:24 +0100

    test: write / update tests

--- apps/account/tests/test_forms/test_edit_user_form.py
+++ apps/account/tests/test_forms/test_edit_user_form.py
@@ -1,10 +1,41 @@
+import tempfile
+from io import BytesIO
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import override_settings
+from PIL import Image
+
 from apps.account.forms import EditUserForm
+from apps.account.forms.edit_user_form import (
+    MAX_PROFILE_PICTURE_DIMENSION,
+    MAX_PROFILE_PICTURE_FILE_SIZE,
+    PROFILE_PICTURE_DIMENSION_ERROR,
+    PROFILE_PICTURE_SIZE_ERROR,
+)
 from apps.core.tests.setup import BaseTestSetUp
 from apps.webpush.models import WebpushInformation
 
 
 class EditUserFormTestCase(BaseTestSetUp):
     form = EditUserForm
+
+    def _build_image_file(self, width=200, height=200):
+        buffer = BytesIO()
+        Image.new("RGB", (width, height), color=(255, 255, 255)).save(buffer, format="PNG")
+        buffer.seek(0)
+        return SimpleUploadedFile("avatar.png", buffer.read(), content_type="image/png")
+
+    def _base_form_data(self, **overrides):
+        data = {
+            "name": self.user.name,
+            "email": self.user.email,
+            "paypal_me_username": self.user.paypal_me_username or "",
+            "wants_to_receive_webpush_notifications": self.user.wants_to_receive_webpush_notifications,
+            "wants_to_receive_payment_reminders": self.user.wants_to_receive_payment_reminders,
+            "wants_to_receive_room_reminders": self.user.wants_to_receive_room_reminders,
+        }
+        data.update(overrides)
+        return data
 
     def test_regular(self):
         new_data = {
@@ -68,3 +99,36 @@ class EditUserFormTestCase(BaseTestSetUp):
 
         self.assertFalse(self.user.wants_to_receive_webpush_notifications)
         self.assertFalse(WebpushInformation.objects.filter(user=self.user).exists())
+
+    def test_profile_picture_upload_saves_file(self):
+        form_data = self._base_form_data()
+        image_file = self._build_image_file()
+
+        with tempfile.TemporaryDirectory() as tmp_media_root, override_settings(MEDIA_ROOT=tmp_media_root):
+            form = self.form(instance=self.user, data=form_data, files={"profile_picture": image_file})
+            self.assertTrue(form.is_valid())
+            form.save()
+
+            self.user.refresh_from_db()
+            self.assertTrue(self.user.profile_picture)
+            stored_name = self.user.profile_picture.name
+            self.assertTrue(self.user.profile_picture.storage.exists(stored_name))
+
+    def test_profile_picture_size_validation(self):
+        form_data = self._base_form_data()
+        image_file = self._build_image_file()
+        image_file.size = MAX_PROFILE_PICTURE_FILE_SIZE + 1
+
+        form = self.form(instance=self.user, data=form_data, files={"profile_picture": image_file})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["profile_picture"][0], PROFILE_PICTURE_SIZE_ERROR)
+
+    def test_profile_picture_dimension_validation(self):
+        form_data = self._base_form_data()
+        image_file = self._build_image_file(
+            width=MAX_PROFILE_PICTURE_DIMENSION + 300, height=MAX_PROFILE_PICTURE_DIMENSION + 300
+        )
+
+        form = self.form(instance=self.user, data=form_data, files={"profile_picture": image_file})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["profile_picture"][0], PROFILE_PICTURE_DIMENSION_ERROR)
--- apps/account/tests/test_views/test_user_update_view.py
+++ apps/account/tests/test_views/test_user_update_view.py
@@ -1,12 +1,23 @@
 import http
+import tempfile
+from io import BytesIO
 
+from django.core.files.base import ContentFile
+from django.test import override_settings
 from django.urls import reverse
+from PIL import Image
 
 from apps.account.views import UserDetailView, UserUpdateView
 from apps.core.tests.setup import BaseTestSetUp
 
 
 class UserUpdateViewTestCase(BaseTestSetUp):
+    def _build_image_bytes(self, width=100, height=100):
+        buffer = BytesIO()
+        Image.new("RGB", (width, height), color=(255, 255, 255)).save(buffer, format="PNG")
+        buffer.seek(0)
+        return buffer.getvalue()
+
     def test_get_regular(self):
         client = self.reauthenticate_user(self.user)
         response = client.get(reverse("account:update", kwargs={"pk": self.user.id}))
@@ -39,3 +50,22 @@ class UserUpdateViewTestCase(BaseTestSetUp):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, new_name)
+
+    def test_profile_picture_delete(self):
+        with tempfile.TemporaryDirectory() as tmp_media_root, override_settings(MEDIA_ROOT=tmp_media_root):
+            self.user.profile_picture.save(
+                "avatar.png",
+                ContentFile(self._build_image_bytes()),
+                save=True,
+            )
+            file_name = self.user.profile_picture.name
+            self.assertTrue(self.user.profile_picture.storage.exists(file_name))
+
+            client = self.reauthenticate_user(self.user)
+            response = client.post(reverse("account:profile-picture-delete"), follow=True)
+            self.assertEqual(response.status_code, http.HTTPStatus.OK)
+            self.assertIn(UserUpdateView.template_name, response.template_name)
+
+            self.user.refresh_from_db()
+            self.assertFalse(self.user.profile_picture)
+            self.assertFalse(self.user.profile_picture.storage.exists(file_name))
