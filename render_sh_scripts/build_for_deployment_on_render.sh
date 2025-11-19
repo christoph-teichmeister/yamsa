dfdb9e66847ddd03ca555ad98e87a7c543bafca3
christoph.teichmeister <christoph.teichmeister@gmail.com>
2025-11-19 12:42:56 +0100

    feat: update build_for_deployment_on_render.sh

--- render_sh_scripts/build_for_deployment_on_render.sh
+++ render_sh_scripts/build_for_deployment_on_render.sh
@@ -7,11 +7,13 @@ set -o errexit
 echo "" && echo "pip install --upgrade pip && pip install -U pip uv"
 pip install --upgrade pip && pip install -U pip uv
 
-echo "" && echo "uv export --locked --format=requirements.txt --no-dev > /tmp/requirements.txt"
-uv export --locked --format=requirements.txt --no-dev --output-file /tmp/requirements.txt
+echo "" && echo "uv export --locked --format=requirements.txt --no-dev --no-emit-project > /tmp/requirements.txt"
+uv export --locked --format=requirements.txt --no-dev --no-emit-project --output-file /tmp/requirements.txt
 
 echo "" && echo "pip install --no-cache-dir -r /tmp/requirements.txt"
 pip install --no-cache-dir -r /tmp/requirements.txt
+echo "" && echo "pip install --no-cache-dir ."
+pip install --no-cache-dir .
 rm /tmp/requirements.txt
 
 echo "" && echo "python manage.py collectstatic --no-input"
