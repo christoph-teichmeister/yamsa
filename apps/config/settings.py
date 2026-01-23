83fcd841c0ec9f9d06ee388e4104eb37e5b3d4d9
christoph.teichmeister <christoph.teichmeister@gmail.com>
2026-01-23 16:32:33 +0100

    chore: add `django-webpack-loader` integration and configure settings
    - Update dependencies and lock file to include `django-webpack-loader`.
    - Configure `WEBPACK_LOADER` in settings for bundle management.
    - Adjust installed apps to include `webpack_loader`.

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -135,8 +135,9 @@ THIRD_PARTY_APPS = (
     "cloudinary",
     "cloudinary_storage",
     "django_extensions",
-    "django_pony_express",
     "django_minify_html",
+    "django_pony_express",
+    "webpack_loader",
 )
 
 LOCAL_APPS = (
@@ -249,6 +250,16 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
+
+WEBPACK_LOADER = {
+    "DEFAULT": {
+        "BUNDLE_DIR_NAME": "bundles/",
+        "STATS_FILE": os.path.join(APPS_DIR, "static", "bundles", "webpack-stats.json"),
+        "POLL_INTERVAL": 0.1,
+        "IGNORE": [r".+\.hot-update.js"],
+        "LOADER_CLASS": "apps.config.custom_webpack_loader.NormalizedWebpackLoader",
+    }
+}
 
 # MEDIA
 # ------------------------------------------------------------------------------
